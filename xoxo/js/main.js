f001a142248434f33f8ef8720fcb237e9ce2c51e
kirillstan1221 <45363441+kirillstan1221@users.noreply.github.com>
2020-04-23 22:54:23 +0600

    Adapt 1

--- xoxo/js/main.js
+++ xoxo/js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('.header_burger').click(function (event) {
+        $('.header_burger, .header_menu').toggleClass('active');
+    });
+    
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
